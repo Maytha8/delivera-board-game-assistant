02344ec6f8b1a4ef938370bf036e4ff9015216e3
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-10-31 13:53:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+function show_compact_number (num: number) {
+    if (num == 1) {
+    	
+    } else if (num == 2) {
+    	
+    } else if (num == 3) {
+    	
+    } else if (num == 4) {
+    	
+    } else if (num == 5) {
+    	
+    } else if (num == 6) {
+    	
+    } else if (num == 7) {
+    	
+    } else if (num == 8) {
+    	
+    } else if (num == 9) {
+    	
+    } else if (num == 10) {
+    	
+    } else if (num == 11) {
+    	
+    } else if (num == 12) {
+    	
+    } else {
+        basic.showNumber(0)
+    }
+}
+function init () {
+    players = 2
+}
+input.onButtonPressed(Button.B, function () {
+	
+})
+let players = 0
+init()
+basic.showString("Delivera")
+basic.forever(function () {
+	
+})
